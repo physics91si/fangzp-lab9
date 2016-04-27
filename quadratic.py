96f77103abc412a0cf0781c4e5637c2936fedef6
Zhanpei Fang <zhanpei@stanford.edu>
2016-04-26 18:23:57 -0700

    Lab 9 uploaded

--- quadratic.py
+++ quadratic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import math
 
 #Python script that returns the roots of a quadratic equation
 #of the form a*x^2 + b*x + c = 0
@@ -14,11 +15,28 @@ def main():
     print ("This is x2: %f" %x2)
 
 
-def find_roots(a,b,c):
-    mid = b^2 - 4*a*c
-    sqrt_mid = mid^(1/2)
-    x1 = (-b + sqrt_mid)/2*a
-    x2 = (-b - sqrt_mid)/2*a
+def find_roots(a,b,c): #Fixed by changing b^2 to b**2, sqrt_mid = mid**(1/2) to math.sqrt(mid)
+    try:
+        float(a)
+        float(b)
+        float(c)
+    except TypeError:
+        print "Not a valid input, please enter three numerical values"
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    try:
+        1/a
+    except ZeroDivisionError:
+        print "Zero leading coefficient; not a quadratic"
+    mid = b**2 - 4*a*c
+    try:
+        math.sqrt(mid)
+    except ValueError:
+        print "This is a negative root, there are no real solutions."
+    sqrt_mid = math.sqrt(mid)
+    x1 = (-b + sqrt_mid)/(2*a)
+    x2 = (-b - sqrt_mid)/(2*a)
     return x1, x2
 
 
